e9382cb6488e134a8f99187eab3707e215d1843e
Pablo Kebees <pablopetje@gmail.com>
2019-09-21 13:59:31 +0200

    Refactor to use multiple accounts

--- README.md
+++ README.md
@@ -2,29 +2,15 @@
 Fork from [plorry/VegAssist](https://github.com/plorry/VegAssist) to retweet people looking for support going vegan for 5 Minutes 5 Vegans.
 https://5minutes5vegans.org
 
-This bot tracks usage of the term "vegan" on twitter's public stream, and processes each item, looking for exact matches on an array of preset phrases. If there's a match, the bot will retweet.
+This bot tracks usage of the term "vegan" on twitter's public stream, and processes each item, looking for exact matches on an array of preset phrases. This bot can be setup for multiple authorised accounts for the same app, retweeting certain filters to only certain accounts.
 
 ## Installing
 
 1. Clone this repository
 2. Install [node.js and npm](https://nodejs.org)
 3. Run `npm install` in the directory that you cloned this repository into
-4. Create a `settings.js` file with the format below, filling in your Twitter API credentials and Twitter botname:
+4. Copy the `config.example.yarn` and fill in your credentials
 5. Run `node .`
-
-#### `settings.js` format:
-```javascript
-module.exports = {
-  CREDS: {
-    consumer_key:         '...',
-    consumer_secret:      '...',
-    access_token:         '...',
-    access_token_secret:  '...'
-  },
-  FILTERS: [],
-  HANDLE: 'botname'
-}
-```
 
 ## Running the tests
 
@@ -32,4 +18,4 @@ Simply run `npm test`
 
 ## Disabling retweets
 
-Retweets can be disabled for testing purposes by using the argument `--dry-run`, e.g. `node vegassist.js --dry-run`. In dry run mode, matching tweets will be logged to the console but not retweeted.
+Retweets can be disabled for testing purposes by using the argument `--dry-run`, e.g. `node . --dry-run`. In dry run mode, matching tweets will be logged to the console but not retweeted.
--- bot.js
+++ bot.js
@@ -1,60 +1,137 @@
-var Twit = require('twit');
-var settings = require('./settings.js');
-var TweetFilter = require('./lib/filter');
-var util = require('./lib/util');
-var path = require('path');
-var fs = require('fs');
-var pg = require('pg');
+var Twitter = require('twit')
+var TweetFilter = require('./lib/filter')
+var util = require('./lib/util')
+var fs = require('fs')
+var yaml = require('js-yaml')
+var request = require('request')
 
-// Declare your own Twitter app credentials here, if duplicating
-var T = new Twit(settings.CREDS);
-// Set a delay time constant of 2 minutes - RTs will be delayed by this amount
-var DELAYTIME = 1000 * 60 * 2;
-// Load filters from all files in the filters directory
-var settingsFilteredTerms = settings.FILTERED_TERMS || [];
-var settingsFilters = settings.FILTERS || [];
+// loead the config or bust
+try {
+    var config = yaml.safeLoad(fs.readFileSync('config.yaml', 'utf8'))
+} catch(e) {
+    throw 'Unable to load config file: ' + e
+}
 
-var filter = new TweetFilter('filters', settingsFilteredTerms, settingsFilters);
-// Whenever the Twitter stream notifies us of a new Tweet with the term 'vegan' (or its international equivalents), we handle it!
-var stream = T.stream('statuses/filter', { track: util.trackedTerms });
-// Run with option '--dry-run' to disable retweeting and instead log matches to console
-var isDryRun = process.argv[2] === '--dry-run';
-// Use a different log file for dry-run
-var logFile = path.join(__dirname, (isDryRun ? 'matches.dry-run' : 'matches') + '.json');
-// Check that we can write to disk, so that we can log it upfront and safely ignore errors later
-// This allows the bot to function even if it doesn't have write access to the disk
-fs.appendFile(logFile, "", function(err) {
-    if (err) { console.warn("Unable to write to log file: " + path.relative(__dirname, logFile)); }
-})
+// fail if there are no users
+if(!config.users.length)
+    throw 'Config needs to contain at least one user'
 
-console.log("Loaded filters: " + Object.keys(filter.filters).join(", "));
+DELAYTIME = config.delaytime || 2 * 60 * 1000
 
-console.log("Tracking terms: " + util.trackedTerms.join(", "));
+var users = []
 
-stream.on('connect', function (response) {
-    console.log("Connecting to Twitter..." + (isDryRun ? " (dry run, will not retweet matches)" : ""))
-})
-stream.on('connected', function (response) {
-    console.log("Connected")
-})
-stream.on('reconnect', function (response) {
-    console.log("Reconnecting...")
-})
-stream.on('tweet', function(tweet) {
-    var matches = filter.matches(tweet);
-    if (matches) {
-        logMatches(tweet, matches);
-        if (isDryRun) {
-            console.log(tweet.id_str + ' : ' + tweet.user.screen_name + ' : ' + tweet.text);
-            return;
-        }
-        // positive match; let's retweet!
-        setTimeout(function(){T.post('statuses/retweet/:id', {id: tweet.id_str}, function(err, data, response) {
-            if (err) {
-                console.log(err);
-                return false;
-            }
-            console.log('Retweeted: ' + tweet.id_str);
-        });}, DELAYTIME);
-    }
+// verify that consumer keys are correct
+request.post( {
+        url: 'https://api.twitter.com/oauth2/token',
+        headers: { 'Authorization': 'Basic ' + Buffer.from(config.consumer_key + ':'  + config.consumer_secret).toString('base64') },
+        form: { 'grant_type': 'client_credentials'}
+    }, ( err, res, body ) => {
+        if(err)
+            throw err
+
+        if(res.statusCode==200)
+            setupUsers()
+        else
+            throw 'Unable to verify consumer API keys: ' + JSON.parse(body).errors[0].message
 });
+
+function setupUsers() {
+
+    // create Twitter instances for all users
+    for(let i = 0; i < config.users.length; i++)
+        users.push(new Twitter({
+            consumer_key: config.consumer_key,
+            consumer_secret: config.consumer_secret,
+            access_token: config.users[i].access_token,
+            access_token_secret: config.users[i].access_token_secret,
+        }))
+
+    // verify that all user credentials are correct
+    var userErrors = 0
+    var promises = []
+    for( let i in users )
+        promises.push( users[i].get('account/verify_credentials').catch(e => {
+            var user = i
+            console.error(`Unable to verify user ${user}: ${e}`) 
+            userErrors ++
+        }))
+
+    Promise.all(promises).then(() => {
+        
+        if( userErrors )
+            console.error('Not all users were verified, terminating')
+        else
+            setupStream()
+    
+    })
+
+}
+
+function setupStream() {
+    
+    // many-to-one map of users using each filter
+    var filterUsers = {}
+    for(let i = 0; i < config.users.length; i++)
+        for(let j in config.users[i].filters) {
+            let filter = config.users[i].filters[j]
+            if(filter in filterUsers)
+                filterUsers[filter].push(i)
+            else
+                filterUsers[filter] = [i]
+        }
+
+    // use first user to stream with
+    var T = users[0]
+
+    // setup the filters
+    var filter = new TweetFilter('filters', config.exclude, Object.keys(filterUsers))
+
+    // Whenever the Twitter stream notifies us of a new Tweet with the term 'vegan' (or its international equivalents), we handle it!
+    var stream = T.stream('statuses/filter', { track: util.trackedTerms })
+
+    // Run with option '--dry-run' to disable retweeting and instead log matches to console
+    var isDryRun = process.argv[2] === '--dry-run'
+
+    console.log("Loaded filters: " + Object.keys(filter.filters).join(", "))
+
+    console.log("Tracking terms: " + util.trackedTerms.join(", "))
+
+    stream.on('connect', function (response) {
+        console.log("Connecting to Twitter..." + (isDryRun ? " (dry run, will not retweet matches)" : ""))
+    })
+    stream.on('connected', function (response) {
+        console.log("Connected")
+    })
+    stream.on('reconnect', function (response) {
+        console.log("Reconnecting...")
+    })
+    stream.on('tweet', function(tweet) {
+        var filters = filter.matches(tweet)
+        if (filters.length) {
+
+            if (isDryRun) {
+                console.log(tweet.id_str + ' : ' + tweet.user.screen_name + ' : ' + tweet.text)
+                return
+            }
+            
+            console.log(`Retweeted: ${tweet.id_str} to ${filters.join(', ')} users`)
+
+            for( let i in filters ) {
+                filter = filters[i]
+                for( var j in filterUsers[filter] ) {
+                    user = filterUsers[filter][j]
+                    setTimeout(function(){
+                        users[user].post('statuses/retweet/:id', {id: tweet.id_str}, function(err, data, response) {
+                            if (err) {
+                                console.log(err)
+                                return false
+                            }
+                        });
+                    }, DELAYTIME)
+                }
+
+            }
+
+        }
+    })
+}
