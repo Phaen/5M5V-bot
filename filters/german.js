b5de665d0dfd5b34ef7c7866214301c0066860c9
Sascha Laabs <tumbleweedxl@outlook.com>
2019-09-24 22:50:46 +0200

    improve german filter

--- filters/german.js
+++ filters/german.js
@@ -4,15 +4,19 @@ var track = util.track;
 
 track("vegan");
 
+var adverbs = [
+    ' liebend', ' auch', ' schon', ' manchmal', ' oft', ' eigentlich', 'voll'
+];
+var adverbsRegexSet = adverbs.join('|');
+
 module.exports = [
-    regex ( "ich (will|möchte|sollte) mich vegan ernähren" ),
-    regex ( "ich (will|möchte)( endlich| auch| gerne| bald)? vegan (werden|sein|leben|essen)" ),
-    regex ( "ich (will|möchte) wieder vegan (sein|werden|leben|essen)" ),
-    regex ( "ich überlege( auch)? vegan zu werden" ),
-    regex ( "ich wäre( liebend)? gerne vegan" ),
-    regex ( "ich würde( ja)? gerne vegan (werden|sein|leben|essen)" ),
-    regex ( "ich hab voll bock vegan zu werden" ),
-    regex ( "ich versuche( jetzt)? auch vegan zu werden" ),
-    regex ( "ich versuche( auch)? gerade vegan zu werden" ),
-    regex ( "ich würde mich( eigentlich)? gerne vegan ernähren" )
+    regex ("([I|i]ch )?(will|möchte|sollte) mich vegan ernähren"),
+    regex ("([I|i]ch )?(will|möchte)( endlich| auch| gerne| bald)? vegan (werden|sein|leben|essen)"),
+    regex ("([I|i]ch )?(will|möchte) wieder vegan (sein|werden|leben|essen)"),
+    regex ("([I|i]ch )?überlege(" + adverbsRegexSet + ")? vegan zu werden"),
+    regex ("([I|i]ch )?wäre(" + adverbsRegexSet + ")? gerne vegan"),
+    regex ("([I|i]ch )?würde( ja)?(" + adverbsRegexSet + ") gerne vegan (werden|sein|leben|essen)"),
+    regex ("([I|i]ch )?hab(" + adverbsRegexSet + ") bock vegan zu werden"),
+    regex ("([I|i]ch )?versuche( " + adverbsRegexSet + ")?( gerade)? vegan zu werden"),
+    regex ("([I|i]ch )?würde mich( " + adverbsRegexSet + ")? gerne vegan ernähren")
 ]
