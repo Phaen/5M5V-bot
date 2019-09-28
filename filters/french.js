40eadc6725e6db963b79a6325a4b84d3416dd900
Pablo Kebees <pablopetje@gmail.com>
2019-09-28 12:30:51 +0200

    v1.5.0

--- filters/french.js
+++ filters/french.js
@@ -4,13 +4,30 @@ var track = util.track;
 
 track("végétalien", "végétalienne", "vegan", "végane");
 
+var vegan = "(totalement |complètement |entièrement |intégralement )?(vegan|végan|végane|végétalien|végétalienne)"
+var becomeVegan = "(devenir|être) " + vegan
+var absolutly = "(absolument |à tout prix |totalement |sérieusement )?"
+
 module.exports = [
-    regex( "je deviens (végétalien|végétalienne|vegan|végane)" ),
-    regex( "je vais (devenir|être) (végétalien|végétalienne|vegan|végane)" ),
-    regex( "je veux( bien)? (devenir|être) (végétalien|végétalienne|vegan|végane)" ),
-    regex( "je veux (devenir|être)( totalement)? (végétalien|végétalienne|vegan|végane)" ),
-    regex( "je suis( vraiment)? motivé à (devenir|être) (végétalien|végétalienne|vegan|végane)" ),
-    regex( "j'aimerais (devenir|être) (végétalien|végétalienne|vegan|végane)" ),
-    regex( "j'hésite( de plus en plus)? à devenir (végétalien|végétalienne|vegan|végane)" ),
-    regex( "je crois que je vais (devenir|être) (végétalien|végétalienne|vegan|végane)" )
+    // Ask for help
+    regex( "j('|e )(ai|aurais|vais( avoir)?) (absolument |sérieusement )?(avoir )?besoin d(e |')(soutien|conseils|informations|renseignements|aide|assistance|un guide|un coup de (main|pouce)) (pour |afin d(e |'))(essayer d(e |')|pouvoir )?" + becomeVegan ),
+    regex( "j(e |')(veux|voudrais|souhaite|souhaiterais|aimerais|cherche à) (avoir|obtenir|recueillir) (des (informations|renseignements|conseils)|de l'aide|d'assistance|un coup de (main|pouce)|un guide) (pour |afin d(e |'))(essayer d(e |')|pouvoir )?" + becomeVegan ),
+    regex( "(aidez|aide)(-| )moi à " + becomeVegan ),
+    regex( "(pouvez|peux|pourriez|pourrais)(-| )(vous|tu) (m'|me )(aider|conseiller|assister|guider) (à|pour) " + becomeVegan ),
+
+    // Want to become vegan
+    regex( "je (veux|voudrais|souhaite|souhaiterais|cherche|viens de (me )?décider|pense( vouloir)?|suis entrain (de penser|d'essayer)) (de |à |d')?" + absolutly + "(de |à |d')?" + becomeVegan ),
+    regex( "j'(espère|envisage|essaye|ai (envie|décidé)|aimerais|hésite) " + absolutly + "(de |à |d')?" + absolutly + becomeVegan ), 
+    
+    // I went vegan
+    regex( "je (suis devenu(e)?|(vais|viens de) devenir) " + vegan ),
+    
+    // I wish I were vegan
+    regex( "j'aurais (voulu|souhaité|aimé) " + becomeVegan ),
+    regex( "je (m'|me )(imagine|vois|verrais) ((assez |plutôt )?bien |déjà )?" + becomeVegan ),
+
+    // I'm having a hard time staying vegan
+    regex( "je n'arrive(rais)? (plus|pas) à (rester|(continuer à )?être) (plus longtemps )?" + vegan + "( plus longtemps)?" ),
+    regex( "j'ai (de plus en plus de|du) mal à (rester|(continuer à )?être) (plus longtemps )?" + vegan + "( plus longtemps)?" ),
+    regex( "je ne sais pas( pour)? combien de temps( encore)? je (vais( pouvoir)?|peux|pourrais) (encore )?(rester|(continuer à )?être) " + vegan )
 ]
