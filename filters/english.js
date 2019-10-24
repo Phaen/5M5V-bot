360131221f3258d4c853911fde2fd7c675ea83d5
Pablo Kebees <pablopetje@gmail.com>
2019-10-24 21:56:11 +0200

    More general and aggressive filters

--- filters/english.js
+++ filters/english.js
@@ -16,17 +16,31 @@ var recidivismAdjectives = [
 ];
 var recidivismAdjectivesRegexSet = recidivismAdjectives.join('|');
 
+var verbsPres = [ 'going', 'becoming( a)?', 'being( a)?', 'staying( a)?' ];
+var verbsPresRegexSet = verbs.join('|');
+
+var verbs = [ 'be( a)?', 'become( a)?', 'go', 'turn', '(try|consider) ' + verbsPresRegexSet ];
+var verbsRegexSet = verbs.join('|');
+
 module.exports = [
-    regex( "help me (be( a)?|become( a)?|go) #?vegan" ),
-    regex( "i (" + adverbsRegexSet + ")? ?(want to|wanna|would like to) (be( a)?|become( a)?|go) #?vegan" ),
-    // forcing this to be at the start of the tweet is a simple hack for excluding things like "don't tell me I should go vegan"
-    regex( "^(i think)? ?i (" + adverbsRegexSet + ")? ?(should) (go|be) #?vegan" ),
-    regex( "i (" + adverbsRegexSet + ")? ?(will|do)? ?(need|want) help (going|becoming( a)?|being( a)?|staying( a)?) #?vegan" ),
-    regex( "i (" + adverbsRegexSet + ")? ?(want to|wanna|would like to|should) (try) (going|becoming( a)?|being( a)?) #?vegan" ),
-    regex( "i('| a)?m (" + adverbsRegexSet + ")? ?(considering|thinking (about|of)|mulling over|planning on) (going|becoming( a)?|being( a)?) #?vegan" ),
-    regex( "i (" + adverbsRegexSet + ")? ?(wish) i (was|were) #?vegan" ),
-    regex( "i (can|could) (" + adverbsRegexSet + ")? ?(see|picture|imagine)( myself)? (going|being( a)?|becoming( a)?) #?vegan"),
-	// stemming recidivism
-    regex( "i('| a)?m having a (" + recidivismAdjectivesRegexSet + ") time (staying|being) #?vegan"),
-    regex( "i? ?don'?t know how much longer i( can| will|'ll) (stay|be) #?vegan")
+    // help me
+    regex( "help me ((in|with) )?(" + verbsPresRegexSet + ") #?vegan" ),
+    // i really want to go vegan
+    regex( "i ((" + adverbsRegexSet + ") )?( might)?(might|have to|should|want to|wanna|would (like|love) to) (" + verbsRegexSet + ") #?vegan" ),
+    // i should really go vegan (^ to avoid "don't tell me I should go vegan")
+    regex( "^(i think )?i should ((" + adverbsRegexSet + ") )?(" + verbsRegexSet + ") #?vegan" ),
+    // need help
+    regex( "i ((" + adverbsRegexSet + ") )?(will |do )?(need|want) help (" + verbsPresRegexSet + ") #?vegan" ),
+    regex( "i ((" + adverbsRegexSet + ") )?(want to|wanna|would like to|should) try (" + verbsPresRegexSet + ") #?vegan" ),
+    // considering going vegan
+    regex( "i('| a)?m ((" + adverbsRegexSet + ") )?(wondering|considering|thinking (about|of)|mulling over|planning on) (" + verbsPresRegexSet + ") #?vegan" ),
+    regex( "i('| a)?m ((" + adverbsRegexSet + ") )?(considering| thinking )?(that )?(to|I should) go #?vegan" ),
+    // wish I can vegan
+    regex( "i ((" + adverbsRegexSet + ") )?(wish|hope) i ((could|can) (" + verbsRegexSet + ")?|was|were) #?vegan" ),
+    // can picture going vegan
+    regex( "i (can|could) ((" + adverbsRegexSet + ") )?(see|picture|imagine)( myself| me)? (" + verbsPresRegexSet + ") #?vegan"),
+    // stemming recidivism
+    regex( "i('| a)?m (having a (" + recidivismAdjectivesRegexSet + ") time (to stay|staying|being) #?vegan"),
+    regex( "don'?t know how much longer i( can| will|'ll) (stay|be) #?vegan")
+
 ]
