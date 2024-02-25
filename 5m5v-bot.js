e158557cb7b46fb3e43198abd5c36db1f6063d61
Jérémy Touati <sielver@gmail.com>
2024-02-25 19:38:06 +0100

    Use new tweet streaming function

--- 5m5v-bot.js
+++ 5m5v-bot.js
@@ -1,27 +1,46 @@
 #!/usr/bin/env node
 
-const fs = require('fs')
-const yaml = require('js-yaml')
-const Twitter = require('./lib/twitter')
-const TweetFilter = require('./lib/filter')
+const fs = require('fs');
+const yaml = require('js-yaml');
+const { Rettiwt } = require('@jeto314/rettiwt-api');
+const TweetFilter = require('./lib/filter');
+const util = require('./lib/util');
 
-const isDryRun = process.argv[2] === '--dry-run'
-
-let config = {}
-
+let config = {};
 try {
-  config = yaml.safeLoad(fs.readFileSync('5m5v-config.yaml', 'utf8'))
+  config = yaml.safeLoad(fs.readFileSync('5m5v-config.yaml', 'utf8'));
 } catch (error) {
-  throw `Unable to load config file: ${error.message}`
+  throw `Unable to load config file: ${error.message}`;
 }
 
-const languages = config.users.map(user => user.language)
+const languages = config.users.map(user => user.language);
+const retweetDelay = config.delaytime || 2 * 60 * 1000;
+const isDryRun = process.argv[2] === '--dry-run';
 
-const twitter = new Twitter(config.users, new TweetFilter(config.exclude, languages))
+const tweetFilter = new TweetFilter(config.exclude, languages);
+const rettiwt = new Rettiwt({ apiKey: config.users[0].api_key });
 
-console.log(isDryRun ? 'Looking for new tweets (dry run)...' : 'Looking for new tweets...')
+console.log(isDryRun ? 'Looking for new tweets (dry run)...' : 'Looking for new tweets...');
 
-twitter.searchAndRetweet({
-  retweetDelay: config.delaytime || 2 * 60 * 1000,
-  isDryRun,
-})
+(async () => {
+  for await (const tweet of rettiwt.tweet.stream({ includeWords: [util.trackedTerms.map(term => `"${term}"`).join(' OR ')] }, 40 * 1000)) {
+    const matchingLanguages = tweetFilter.matches(tweet) || [];
+
+    for (const language of matchingLanguages) {
+      await new Promise(resolve => setTimeout(resolve, retweetDelay));
+
+      try {
+        if (!isDryRun) {
+          const apiKey = config.users.find(user => user.language === language)?.api_key ?? null;
+          const rettiwt = new Rettiwt({ apiKey });
+
+          await rettiwt.tweet.retweet(tweet.id)
+        }
+
+        console.log(`Retweeted tweet ${tweet.id} in ${language}:\n${tweet.fullText}`);
+      } catch (error) {
+        console.error(`Unable to retweet ${tweet.id} in ${language}: ${error.message}`);
+      }
+    }
+  }
+})();
