a32be80819e41e5f8a258bd668d8a8cf6e556a6f
Jérémy Touati <sielver@gmail.com>
2024-03-01 22:07:56 +0100

    Exit script after fetching config (test)

--- 5m5v-bot.js
+++ 5m5v-bot.js
@@ -13,6 +13,8 @@ try {
   throw `Unable to load config file: ${error.message}`;
 }
 
+process.exit();
+
 const languages = config.users.map(user => user.language);
 const pollingIntervalMs = 40 * 1000;
 const retweetDelayMs = config.delaytime || 2 * 60 * 1000;
